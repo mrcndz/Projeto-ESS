fe08ee9e1e6c0902a3248dbf2ef89c32a1387cd9
mrcndz <jrmarcondesb@gmail.com>
2023-08-17 11:20:08 -0300

    feat(Followers): added more tests and features

--- backend/src/database/users.ts
+++ backend/src/database/users.ts
@@ -22,7 +22,7 @@ let users: User[] = [
     lastName: "Silva",
     pronouns: "ele/dele",
     bio: "Olá! Sou Fulano Silva e estou explorando o mundo da tecnologia.",
-    followers: [],
+    followers: [2, 3],
     following: [],
     blocked: []
   },
@@ -36,7 +36,7 @@ let users: User[] = [
     pronouns: "ele/dele",
     bio: "Olá! Sou Ciclano Silva e estou explorando o mundo da tecnologia.",
     followers: [3],
-    following: [],
+    following: [1],
     blocked: []
   },
   {
@@ -49,7 +49,7 @@ let users: User[] = [
     pronouns: "ele/dele",
     bio: "Olá! Sou Beltrano Silva e estou explorando o mundo da tecnologia.",
     followers: [3],
-    following: [2],
+    following: [2, 1],
     blocked: []
   },
   {
